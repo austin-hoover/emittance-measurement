5033fbff01541ecd30c941ffc40d386113db82bb
austin-hoover <ahoover7@tennessee.edu>
2021-03-30 12:07:26 -0400

    Comment out

--- test_set_live_quads.py
+++ test_set_live_quads.py
@@ -48,14 +48,14 @@ print 'Final field strength (live)  = {} [T/m]'.format(init_field_strength_live)
 
 # Test on multiple quads
 #------------------------------------------------------------------------------
-controller.restore_default_optics()
-mux, muy = controller.get_ref_ws_phases()
-mux += radians(5.0)
-controller.set_ref_ws_phases(mux, muy, beta_lims, verbose=1)
-controller.set_betas_at_target(design_betas_at_target, beta_lim_after_ws24, verbose=1)
-
-model_field_strengths = controller.get_field_strengths(all_quad_ids)
-live_field_strengths = controller.get_live_field_strengths(all_quad_ids)
-
-for k_model, k_live in zip(model_field_strengths, live_field_strengths):
-    print 'k_model, k_live = {:.4f}, {:.4f}'.format(k_model, k_live)
+#controller.restore_default_optics()
+#mux, muy = controller.get_ref_ws_phases()
+#mux += radians(5.0)
+#controller.set_ref_ws_phases(mux, muy, beta_lims, verbose=1)
+#controller.set_betas_at_target(design_betas_at_target, beta_lim_after_ws24, verbose=1)
+#
+#model_field_strengths = controller.get_field_strengths(all_quad_ids)
+#live_field_strengths = controller.get_live_field_strengths(all_quad_ids)
+#
+#for k_model, k_live in zip(model_field_strengths, live_field_strengths):
+#    print 'k_model, k_live = {:.4f}, {:.4f}'.format(k_model, k_live)
