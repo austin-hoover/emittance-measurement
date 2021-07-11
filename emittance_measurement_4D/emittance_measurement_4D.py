8bee5d7f12ccd0fbfe5c4209fa986f71dc15c7cc
austin-hoover <ahoover7@tennessee.edu>
2021-07-11 15:10:36 -0400

    Add listener to n_steps

--- emittance_measurement_4D/emittance_measurement_4D.py
+++ emittance_measurement_4D/emittance_measurement_4D.py
@@ -19,8 +19,13 @@ from javax.swing import JComboBox
 from javax.swing import JFrame
 from javax.swing import JLabel
 from javax.swing import JPanel
+from javax.swing import JTable
 from javax.swing import JTextField
+from javax.swing import JFormattedTextField
 from javax.swing.event import DocumentListener
+from javax.swing.table import AbstractTableModel
+from java.text import NumberFormat
+from java.text import DecimalFormat
 
 from xal.extension.widgets.plot import BasicGraphData
 from xal.extension.widgets.plot import FunctionGraphsJPanel
@@ -61,7 +66,7 @@ class GUI:
         time_panel.add(time_text.getTimeTextField(), BorderLayout.CENTER)
         self.frame.add(time_panel, BorderLayout.SOUTH)
 
-        # Text fields panel
+        # Model calculation panel
         #------------------------------------------------------------------------
         # Labels
         ref_ws_id_label = JLabel('Ref. wire-scanner')
@@ -75,9 +80,14 @@ class GUI:
         self.ref_ws_id_dropdown = JComboBox(ws_ids)
         self.energy_text_field = JTextField('1.0', text_field_width)
         self.phase_coverage_text_field = JTextField('180.0', text_field_width)
-        self.n_steps_text_field = JTextField('12', text_field_width)
+        
+        formatter = NumberFormat.getIntegerInstance()
+        formatter.setGroupingUsed(False)
+        self.n_steps_text_field = JFormattedTextField(formatter)
+        self.n_steps_text_field.setValue(12)
+        
         self.max_beta_text_field = JTextField('40.0', text_field_width)
-        self.calculate_model_optics_button = JButton('Calculate model optics (all steps)')
+        self.calculate_model_optics_button = JButton('Calculate model optics for scan')
         
         # Action listeners
         self.energy_text_field.addActionListener(EnergyTextFieldListener(self))
@@ -86,40 +96,47 @@ class GUI:
         self.calculate_model_optics_button.addActionListener(CalculateModelOpticsButtonListener(self))
         
         # Build text fields panel
-        self.text_fields_panel = AlignedLabeledTextFieldsPanel()
+        self.model_calc_panel = AlignedLabeledComponentsPanel()
            
-        self.text_fields_panel.add_row(ref_ws_id_label, self.ref_ws_id_dropdown)
-        self.text_fields_panel.add_row(energy_label, self.energy_text_field)
-        self.text_fields_panel.add_row(phase_coverage_label, self.phase_coverage_text_field)
-        self.text_fields_panel.add_row(n_steps_label, self.n_steps_text_field)
-        self.text_fields_panel.add_row(max_beta_label, self.max_beta_text_field)
+        self.model_calc_panel.add_row(ref_ws_id_label, self.ref_ws_id_dropdown)
+        self.model_calc_panel.add_row(energy_label, self.energy_text_field)
+        self.model_calc_panel.add_row(phase_coverage_label, self.phase_coverage_text_field)
+        self.model_calc_panel.add_row(n_steps_label, self.n_steps_text_field)
+        self.model_calc_panel.add_row(max_beta_label, self.max_beta_text_field)
     
-        # Lower panel
+        # Machine update panel
         #------------------------------------------------------------------------ 
         # Labels
         sleep_time_label = JLabel('Sleep time [s]')
         max_frac_change_label = JLabel('Max. frac. field change')
+        scan_index_label = JLabel('Scan index')
         
         # Components        
         self.sleep_time_text_field = JTextField('0.5', text_field_width)
         self.max_frac_change_text_field = JTextField('0.01', text_field_width)
         self.set_optics_button = JButton('Set live optics')
+        self.quad_settings_table = JTable(QuadSettingsTableModel(self))
+        self.quad_settings_table.setShowGrid(True)
+        
+        n_steps = int(self.n_steps_text_field.getText())
+        scan_indices = list(range(n_steps))
+        self.scan_index_dropdown = JComboBox(scan_indices)
         
         # Action listeners
+        self.n_steps_text_field.addActionListener(NStepsTextFieldListener(self))
         
-        # Build buttons panel
-        self.buttons_panel = JPanel()
-        self.buttons_panel.setLayout(BoxLayout(self.buttons_panel, BoxLayout.Y_AXIS))
+        # Build panel
+        self.machine_update_panel = JPanel()
+        self.machine_update_panel.setLayout(BoxLayout(self.machine_update_panel, BoxLayout.Y_AXIS))
+        temp_panel = AlignedLabeledComponentsPanel()
+        temp_panel.add_row(sleep_time_label, self.sleep_time_text_field)
+        temp_panel.add_row(max_frac_change_label, self.max_frac_change_text_field)  
+        temp_panel.add_row(scan_index_label, self.scan_index_dropdown)
+        self.machine_update_panel.add(temp_panel)
+        self.machine_update_panel.add(self.set_optics_button)
+        self.machine_update_panel.add(self.quad_settings_table.getTableHeader())
+        self.machine_update_panel.add(self.quad_settings_table)
         
-        
-        temp_panel = JPanel()
-        temp_panel.add(sleep_time_label)
-        temp_panel.add(self.sleep_time_text_field)
-        temp_panel.add(max_frac_change_label)
-        temp_panel.add(self.max_frac_change_text_field)
-        self.buttons_panel.add(temp_panel)
-        self.buttons_panel.add(self.set_optics_button)
-        self.buttons_panel.setAlignmentX(Component.LEFT_ALIGNMENT)
         
         # Build left panel
         #------------------------------------------------------------------------    
@@ -128,21 +145,20 @@ class GUI:
         
         label = JLabel('Compute model')
         font = label.getFont()
-        label.setFont(Font(font.name, font.BOLD, int(1.1 * font.size)));
+        label.setFont(Font(font.name, font.BOLD, int(1.05 * font.size)));
         self.left_panel.add(label)
         
-        self.left_panel.add(self.text_fields_panel)
+        self.left_panel.add(self.model_calc_panel)
         
         panel = JPanel()
-        self.calculate_model_optics_button.setAlignmentX(Component.LEFT_ALIGNMENT)
         panel.add(self.calculate_model_optics_button)
         self.left_panel.add(panel)
         
         label = JLabel('Update machine')
-        label.setFont(Font(font.name, font.BOLD, int(1.1 * font.size)));
+        label.setFont(Font(font.name, font.BOLD, int(1.05 * font.size)));
         self.left_panel.add(label)
         
-#         self.left_panel.add(self.buttons_panel)
+        self.left_panel.add(self.machine_update_panel)
         
         self.frame.add(self.left_panel, BorderLayout.WEST)
         
@@ -204,41 +220,43 @@ class GUI:
             'max_frac_change': float(self.max_frac_change_text_field.getText()),
         }
         self.frame.addWindowListener(WindowCloser(self.phase_controller, field_set_kws, self.live))
-        self.frame.show()        
+        self.frame.show()   
         
-
-
-class AlignedLabeledTextFieldsPanel(JPanel):
+        
+# Tables
+#-------------------------------------------------------------------------------      
+class QuadSettingsTableModel(AbstractTableModel):
     
-    def __init__(self):
-        JPanel.__init__(self)
-        self.layout = GroupLayout(self)
-        self.setLayout(self.layout)
-        self.layout.setAutoCreateContainerGaps(True)
-        self.layout.setAutoCreateGaps(True)
-        self.group_labels = self.layout.createParallelGroup()
-        self.group_fields = self.layout.createParallelGroup()
-        self.group_rows = self.layout.createSequentialGroup()
-        self.layout.setHorizontalGroup(self.layout.createSequentialGroup()
-                                  .addGroup(self.group_labels)
-                                  .addGroup(self.group_fields))
-        self.layout.setVerticalGroup(self.group_rows)
+    def __init__(self, gui):
+        self.gui = gui
+        self.phase_controller = gui.phase_controller
+        self.column_names = ['Quad', 'Model [T/m]', 'Live [T/m]']
+        self.nf4 = NumberFormat.getInstance()
+        self.nf4.setMaximumFractionDigits(4)
+        self.nf3 = NumberFormat.getInstance()
+        self.nf3.setMaximumFractionDigits(3)
 
-    def add_row(self, label, field):
-        self.group_labels.addComponent(label)
-        self.group_fields.addComponent(field)
-        self.group_rows.addGroup(
-            self.layout.createParallelGroup()
-                .addComponent(label)
-                .addComponent(
-                    field, 
-                    GroupLayout.PREFERRED_SIZE, 
-                    GroupLayout.DEFAULT_SIZE, 
-                    GroupLayout.PREFERRED_SIZE
-                )
-        )
+    def getValueAt(self, row, col):
+        quad_id = self.phase_controller.ind_quad_ids[row]
+        if col == 0:
+            return quad_id
+        elif col == 1:
+            return self.phase_controller.get_field(quad_id, 'model')
+        elif col == 2:
+            return self.phase_controller.get_field(quad_id, 'live')
         
+    def getColumnCount(self):
+        return len(self.column_names)
+
+    def getRowCount(self):
+        return len(self.phase_controller.ind_quad_ids)
+    
+    def getColumnName(self, col):
+        return self.column_names[col]
         
+    
+# Listeners
+#-------------------------------------------------------------------------------
 class EnergyTextFieldListener(ActionListener):
     """Update the beam kinetic energy from the text field; retrack; replot."""
     def __init__(self, gui):
@@ -269,6 +287,19 @@ class RefWsIdTextFieldListener(ActionListener):
         print 'Updated ref_ws_id to {}'.format(self.phase_controller.ref_ws_id)
         
         
+class NStepsTextFieldListener(ActionListener):
+    """Update a dropdown menu based on the n_steps text field."""
+    def __init__(self, gui):
+        self.gui = gui
+    
+    def actionPerformed(self, event):
+        n_steps = float(self.gui.n_steps_text_field.getText())
+        n_steps = int(n_steps)
+        self.gui.scan_index_dropdown.removeAllItems()
+        for scan_index in range(n_steps):
+            self.gui.scan_index_dropdown.addItem(scan_index)
+        
+        
 class CalculateModelOpticsButtonListener(ActionListener):
     """Calculate the model optics to obtain selected phase advances."""
     def __init__(self, gui):
@@ -280,8 +311,10 @@ class CalculateModelOpticsButtonListener(ActionListener):
         # 2. Run the solver.
         # 3. Print the output.
         raise NotImplementedError
+            
     
-    
+# Plotting
+#-------------------------------------------------------------------------------
 class LinePlotPanel(JPanel):
     """Class for 2D line plots."""
     def __init__(self, xlabel='', ylabel='', title='', n_lines=2):
@@ -312,5 +345,47 @@ class LinePlotPanel(JPanel):
             self.graph.addGraphData(data)        
             
             
+
+# Miscellaneous
+#-------------------------------------------------------------------------------
+class AlignedLabeledComponentsPanel(JPanel):
+    
+    def __init__(self):
+        JPanel.__init__(self)
+        self.layout = GroupLayout(self)
+        self.setLayout(self.layout)
+        self.layout.setAutoCreateContainerGaps(True)
+        self.layout.setAutoCreateGaps(True)
+        self.group_labels = self.layout.createParallelGroup()
+        self.group_components = self.layout.createParallelGroup()
+        self.group_rows = self.layout.createSequentialGroup()
+        self.layout.setHorizontalGroup(self.layout.createSequentialGroup()
+                                  .addGroup(self.group_labels)
+                                  .addGroup(self.group_components))
+        self.layout.setVerticalGroup(self.group_rows)
+
+    def add_row(self, label, component):
+        self.group_labels.addComponent(label)
+        self.group_components.addComponent(component)
+        self.group_rows.addGroup(
+            self.layout.createParallelGroup()
+                .addComponent(label)
+                .addComponent(
+                    component, 
+                    GroupLayout.PREFERRED_SIZE, 
+                    GroupLayout.DEFAULT_SIZE, 
+                    GroupLayout.PREFERRED_SIZE
+                )
+        )
+            
+            
+            
+            
 gui = GUI()
 gui.launch()
+
+
+
+
+
+# When model or live quads are updated: call table.getModel().fireTableDataChanged()
