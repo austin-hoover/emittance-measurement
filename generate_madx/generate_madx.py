3e0ce0fd6c5727957776d81febdabb16685416cb
austin-hoover <ahoover7@tennessee.edu>
2021-08-10 14:11:47 -0400

    Bring  folder up to date

--- generate_madx/generate_madx.py
+++ generate_madx/generate_madx.py
@@ -2,25 +2,40 @@
 
 from java.io import File
 from xal.extension.extlatgen import AbstractDeviceDataSource
-from xal.extension.extlatgen import MadGenerator
 from xal.extension.extlatgen import MadXGenerator
 from xal.model.probe import Probe
+from xal.model.probe.traj import Trajectory
+from xal.service.pvlogger.sim import PVLoggerDataSource
 from xal.sim.scenario import AlgorithmFactory
 from xal.sim.scenario import ProbeFactory
+from xal.sim.scenario import Scenario
+from xal.sim.sync import SynchronizationException
 from xal.smf import Accelerator
 from xal.smf import AcceleratorSeq 
 from xal.smf.data import XMLDataManager
+from xal.tools.beam import CovarianceMatrix
+from xal.tools.beam import PhaseVector
+from xal.tools.beam import Twiss
+from xal.tools.beam.calc import CalculationsOnBeams
+from xal.tools.beam.calc import CalculationsOnRings
+
+
+pvloggerid = 48842340
+kin_energy = 1e9
 
 accelerator = XMLDataManager.loadDefaultAccelerator()
 sequence = accelerator.getComboSequence('RTBT')
+tracker = AlgorithmFactory.createEnvelopeTracker(sequence)
+tracker.setUseSpacecharge(False) 
+probe = ProbeFactory.getEnvelopeProbe(sequence, tracker)
+probe.setBeamCurrent(0.0) 
+probe.setKineticEnergy(kin_energy)
+probe.initialize()
 
-algorithm = AlgorithmFactory.createEnvelopeTracker(sequence)
-algorithm.setUseSpacecharge(False) # might be unnecessary
-probe = ProbeFactory.getEnvelopeProbe(sequence, algorithm)
-probe.setBeamCurrent(0.0) # might be unnecessary
-
+sequence = accelerator.getComboSequence('RTBT')
 mad_generator = MadXGenerator([sequence], probe)
+# data_source = AbstractDeviceDataSource.getPVLoggerDataSourceInstance(pvloggerid)
 data_source = AbstractDeviceDataSource.getDesignDataSourceInstance()
-mad_generator.createMadInput(data_source, File('rtbt.mad'))
+mad_generator.createMadInput(data_source, File('rtbt.madx'))
 
 exit()
