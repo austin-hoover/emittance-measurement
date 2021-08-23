6c18aa10fa17a89a9e3e08619f44c7be008e1cdc
austin-hoover <ahoover7@tennessee.edu>
2021-08-23 18:15:41 -0400

    Continue sensitivity study

--- emittance_measurement_4D/sensitivity/test.py
+++ emittance_measurement_4D/sensitivity/test.py
@@ -1,3 +1,7 @@
+"""
+Observe the effect of changing the lattice optics on the measurement 
+sensitivity.
+"""
 from __future__ import print_function
 import os
 import random
@@ -13,37 +17,35 @@ from lib import utils
 from lib.least_squares import lsq_linear 
 
 
-# For printing
-header = ''.join(['error | fail rate | ',
-                  'epsx [mm mrad] | ',
-                  'epsy [mm mrad] | ',
-                  'eps1 [mm mrad] | ',
-                  'eps2 [mm mrad] '])
-fstr = ' | '.join(['{:<5}', '{:<9.3f}'] + 4 * ['{:<5.2f} +/- {:.2f}'])
+# Setup
+n_trials = 10000
+# rms_frac_size_errors = utils.linspace(0.0, 0.10, 6)
+rms_frac_size_errors = [0, 0.01, 0.05, 0.10, 0.20]
+dmux = utils.radians(-10.0)
+dmuy = utils.radians(+45.0)
+ws_ids = optics.RTBT_IND_QUAD_IDS[1:]
+controller = optics.PhaseController(ref_ws_id=ws_ids[-1], sync_mode='live')
+mux0, muy0 = controller.phases('RTBT_Diag:WS24')
 
+
+# For printing
 def print_header():
+    header = ''.join(['error | fail rate | ',
+                      'epsx [mm mrad] | ',
+                      'epsy [mm mrad] | ',
+                      'eps1 [mm mrad] | ',
+                      'eps2 [mm mrad] '])
     print(header)
     print(len(header) * '-')
 
 def print_results(rms_frac_size_error, fail_rate, emittances):
-    emittances = Matrix(emittances)
+    fstr = ' | '.join(['{:<5}', '{:<9.3f}'] + 4 * ['{:<5.2f} +/- {:.2f}'])
     means = utils.mean_cols(emittances)
     stds = utils.std_cols(emittances)
     print(fstr.format(rms_frac_size_error, fail_rate, 
                       means[0], stds[0], means[1], stds[1], 
                       means[2], stds[2],means[3], stds[3]))
     
-# Setup
-n_trials = 10000
-rms_frac_size_errors = [0., 0.01, 0.02, 0.03, 0.04, 0.05]
-ws_ids = optics.RTBT_IND_QUAD_IDS[1:]
-controller = optics.PhaseController(ref_ws_id=ws_ids[-1], sync_mode='live')
-mux0, muy0 = controller.phases('RTBT_Diag:WS24')
-dmux = utils.radians(-10.0)
-dmuy = utils.radians(+45.0)
-print('Initial phases at WS24: mux, muy = {:.3f}, {:.3f}'.format(mux0, muy0))
-print('dmux, dmuy = {:.3f}, {:.3f} [deg]'.format(utils.degrees(dmux), 
-                                                 utils.degrees(dmuy)))
 
 # Create initial matched covariance matrix (uncoupled).
 eps_x = 32.0 # [mm mrad]
@@ -55,22 +57,30 @@ beta_y = controller.init_twiss['beta_y']
 Sigma0 = uncoupled_matched_cov(alpha_x, alpha_y, beta_x, beta_y, eps_x, eps_y)
 
 
-# Record the fail rate vs. error in measured beam size.
 
-def run_and_print_results(controller):
-    print_header()
-    for rms_frac_size_error in rms_frac_size_errors:
-        tmats = [controller.transfer_matrix(ws_id) for ws_id in ws_ids]
-        fail_rate, emittances = run_trials(Sigma0, tmats, n_trials, 
-                                           rms_frac_size_error)
-        print_results(rms_frac_size_error, fail_rate, emittances)
+tmats = [controller.transfer_matrix(ws_id) for ws_id in ws_ids]
+fail_rate, emittances = run_trials(Sigma0, tmats, 1000, 0.10, disp=True)
+print(fail_rate, utils.mean_cols(emittances))
 
 
-run_and_print_results(controller)
-print()
-print('Changing optics.')
-controller.set_ref_ws_phases(mux0 + dmux, muy0 + dmuy, verbose=1)
-print()
-run_and_print_results(controller)
+# # Record the fail rate vs. error in measured beam size.
+
+# def run_and_print_results(controller):
+#     print_header()
+#     for rms_frac_size_error in rms_frac_size_errors:
+#         tmats = [controller.transfer_matrix(ws_id) for ws_id in ws_ids]
+#         fail_rate, emittances = run_trials(Sigma0, tmats, n_trials, 
+#                                            rms_frac_size_error)
+#         print_results(rms_frac_size_error, fail_rate, emittances)
+
+# print('Initial phases at WS24: mux, muy = {:.3f}, {:.3f} [rad]'.format(mux0, muy0))
+# print('dmux, dmuy = {:.3f}, {:.3f} [deg]'.format(utils.degrees(dmux), 
+#                                                  utils.degrees(dmuy)))
+# run_and_print_results(controller)
+# print()
+# print('Changing optics.')
+# controller.set_ref_ws_phases(mux0 + dmux, muy0 + dmuy, verbose=1)
+# print()
+# run_and_print_results(controller)
 
 exit()
--- emittance_measurement_4D/sensitivity/test2.py
+++ emittance_measurement_4D/sensitivity/test2.py
@@ -42,8 +42,15 @@ from lib.least_squares import lsq_linear
 from lib import utils
 
 
-ws_ids = ['RTBT_Diag:WS20', 'RTBT_Diag:WS21', 'RTBT_Diag:WS23', 'RTBT_Diag:WS24']
-controller = optics.PhaseController(ref_ws_id=ws_ids[-1])
+# Setup
+ws_ids = optics.RTBT_WS_IDS[1:]
+n_steps = 10
+dmux_lo = utils.radians(-30.0)
+dmux_hi = utils.radians(+45.0)
+dmuy_lo = utils.radians(-30.0)
+dmuy_hi = utils.radians(+45.0)
+controller = optics.PhaseController(ref_ws_id=ws_ids[-1], sync_mode='live')
+mux0, muy0 = controller.phases(ws_ids[-1])
 
 # Create initial matched covariance matrix (uncoupled).
 eps_x = 32.0 # [mm mrad]
@@ -55,57 +62,43 @@ beta_y = controller.init_twiss['beta_y']
 Sigma0 = uncoupled_matched_cov(alpha_x, alpha_y, beta_x, beta_y, eps_x, eps_y)
 
 
+# Compute condition number for Axy over grid of x and y phase advances.
+#------------------------------------------------------------------------------
+dmuxx = utils.linspace(dmux_lo, dmux_hi, n_steps)
+dmuyy = utils.linspace(dmuy_lo, dmuy_hi, n_steps)
+condition_numbers = Matrix(n_steps, n_steps)
+mean_eps_x = Matrix(n_steps, n_steps)
+mean_eps_y = Matrix(n_steps, n_steps)
+mean_eps_1 = Matrix(n_steps, n_steps)
+mean_eps_2 = Matrix(n_steps, n_steps)
+std_eps_x = Matrix(n_steps, n_steps)
+std_eps_y = Matrix(n_steps, n_steps)
+std_eps_1 = Matrix(n_steps, n_steps)
+std_eps_2 = Matrix(n_steps, n_steps)
 
-# # Compute condition number for Axy over grid of x and y phase advances.
-# #------------------------------------------------------------------------------
-# n_steps = 50
-
-
-# dmux_lo = utils.radians(-45.0)
-# dmux_hi = utils.radians(+60.0)
-# dmuy_lo = utils.radians(-45.0)
-# dmuy_hi = utils.radians(+60.0)
-
-
-# def linspace(start, stop, n_steps):
-#     step = abs(stop - start) / (n_steps - 1)
-#     items = [start]
-#     for _ in range(n_steps - 1):
-#         items.append(items[-1] + step)
-#     return items
-
-
-# dmuxx = linspace(dmux_lo, dmux_hi, n_steps)
-# dmuyy = linspace(dmuy_lo, dmuy_hi, n_steps)
-# condition_numbers = Matrix(n_steps, n_steps)
-
-# print()
-# print('i/N  | j/N  | muy   | mux   | cond(Axy)')
-# print('-----------------------------------------')
-# for j, dmuy in enumerate(dmuyy):    
-#     muy = utils.put_angle_in_range(muy0 + dmuy)
-#     for i, dmux in enumerate(dmuxx):
-#         mux = utils.put_angle_in_range(mux0 + dmux)
-#         controller.set_ref_ws_phases(mux, muy, verbose=0)
-#         tmats = [controller.transfer_matrix(ws_id) for ws_id in ws_ids]
-#         Axx, Ayy, Axy = [], [], []
-#         for M in tmats:
-#             Axx.append([M[0][0]**2, M[0][1]**2, 2*M[0][0]*M[0][1]])
-#             Ayy.append([M[2][2]**2, M[2][3]**2, 2*M[2][2]*M[2][3]])
-#             Axy.append([M[0][0]*M[2][2],  M[0][1]*M[2][2],  M[0][0]*M[2][3],  M[0][1]*M[2][3]])
-#         Axx = Matrix(Axx)
-#         Ayy = Matrix(Ayy)
-#         Axy = Matrix(Axy)
+print()
+print('i/N  | j/N  | mux   | muy   | cond(Axy)')
+print('-----------------------------------------')
+for i, dmuy in enumerate(dmuyy):    
+    muy = utils.put_angle_in_range(muy0 + dmuy)
+    for j, dmux in enumerate(dmuxx):
+        mux = utils.put_angle_in_range(mux0 + dmux)
+        controller.set_ref_ws_phases(mux, muy, verbose=0)
+        tmats = [controller.transfer_matrix(ws_id) for ws_id in ws_ids]
+        Axx, Ayy, Axy = [], [], []
+        for M in tmats:
+            Axx.append([M[0][0]**2, M[0][1]**2, 2*M[0][0]*M[0][1]])
+            Ayy.append([M[2][2]**2, M[2][3]**2, 2*M[2][2]*M[2][3]])
+            Axy.append([M[0][0]*M[2][2],  M[0][1]*M[2][2],  M[0][0]*M[2][3],  M[0][1]*M[2][3]])
         
-#         condition_number = cond(Axy)
-#         condition_numbers.set(i, j, condition_number)
-#         condition_number = 0.
+        condition_number = utils.cond(Axy)
+        condition_numbers.set(i, j, condition_number)
         
-#         fstr = '{}/{} | {}/{} | {:.3f} | {:.3f} | {}'
-#         print(fstr.format(i, n_steps - 1, j, n_steps - 1, muy, mux, condition_number))
+        fstr = '{}/{} | {}/{} | {:.3f} | {:.3f} | {}'
+        print(fstr.format(i, n_steps - 1, j, n_steps - 1, mux, muy, condition_number))
         
-# utils.save_array(condition_numbers.getArray(), 'condition_numbers.dat')
-# utils.save_array(dmuxx, 'phase_devs_x.dat')
-# utils.save_array(dmuyy, 'phase_devs_y.dat')
+utils.save_array(condition_numbers.getArray(), 'condition_numbers.dat')
+utils.save_array(dmuxx, 'phase_devs_x.dat')
+utils.save_array(dmuyy, 'phase_devs_y.dat')
 
 exit()
