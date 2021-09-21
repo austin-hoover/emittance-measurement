f3648daa4d8bb9f395de6af9f950deecd21cb0f0
austin-hoover <ahoover7@tennessee.edu>
2021-09-21 11:44:42 -0400

    Remove old method to restore machine to default state when closing app

--- emittance_measurement_4D/emittance_measurement_4D.py
+++ emittance_measurement_4D/emittance_measurement_4D.py
@@ -68,38 +68,19 @@ class EmittanceMeasurement4D:
         time_panel = JPanel(BorderLayout())
         time_panel.add(time_text.getTimeTextField(), BorderLayout.CENTER)
         self.frame.add(time_panel, BorderLayout.SOUTH)        
-        
-    def get_field_set_kws(self):
-        """Get key words for setting the live quads fields.
-        
-        We need these because the application restores the machine to its original
-        state when it is closed.
-        """
-        panel = self.phase_controller_panel
-        field_set_kws = {
-            'sleep_time': float(panel.sleep_time_text_field.getText()),
-            'max_frac_change': float(panel.max_frac_change_text_field.getText()),
-        }
-        return field_set_kws
 
     def launch(self):
         """Launch the GUI."""
         class WindowCloser(WindowAdapter):
-            def __init__(self, phase_controller, field_set_kws):
-                self.phase_controller = phase_controller
-                self.field_set_kws = field_set_kws
+            
+            def __init__(self):
+                return
                 
             def windowClosing(self, event):
                 """Reset the real machine to its default state before closing window."""
-                #if self.phase_controller.machine_has_changed:
-                    #print 'Restoring machine to default state.'
-                    #self.phase_controller.restore_default_optics('live', **self.field_set_kws)
                 sys.exit(1)
 
-        field_set_kws = self.get_field_set_kws()
-        phase_controller = self.phase_controller_panel.phase_controller
-        window_closer = WindowCloser(phase_controller, field_set_kws)
-        self.frame.addWindowListener(window_closer)
+        self.frame.addWindowListener(WindowCloser())
         self.frame.show() 
         
             
