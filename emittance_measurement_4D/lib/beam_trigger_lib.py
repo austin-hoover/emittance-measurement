400645a303cd27695f539531a06049ae444e9a85
austin-hoover <ahoover7@tennessee.edu>
2022-01-23 20:40:40 -0500

    Simplify beam_trigger_lib.py

--- emittance_measurement_4D/lib/beam_trigger_lib.py
+++ emittance_measurement_4D/lib/beam_trigger_lib.py
@@ -1,114 +1,77 @@
 """Beam trigger library.
 
-This was copied and pasted from another application.
+This was ported from a different application.
 """
+from __future__ import print_function
 import time
-from java.lang import *
+from java.lang import * # Wildcard imports are not allowed in Java 17+
 from xal.extension.scan import WrappedChannel
 
 
-false = Boolean("false").booleanValue()
-true  = Boolean("true").booleanValue()
-null  = None
-
-
 class BeamTrigger:
-	"""Class to trigger the beam.
+    """Class to trigger the beam.
 
-	Note: if the there is an error in the machine, the this class will not trigger
-	the beam. It seems that manually triggering from StartMap will solve the
-	problem.
-	"""
-	def __init__(self,top_level_cotroller = null):
-		self.top_level_cotroller   = top_level_cotroller
-		self.beamTriggerWPV        = null
-		self.testPV                = null
-		self.sleepMeasureTime      = 0.1
-		self.scan_state_controller = null
-		self.fake_scan             = false
-		self.use_trigger           = true
-		
-	def initChannels(self):
-		if(not self.fake_scan):
-			if(self.beamTriggerWPV == null or self.testPV == null):
-				self.beamTriggerWPV = WrappedChannel("ICS_Tim:Gate_BeamOn:SSTrigger")
-				self.testPV = WrappedChannel("MEBT_Diag:BPM01:amplitudeAvg")
-				self.testPV.startMonitor()	
-				time.sleep(2.0)
-		
-	def setScanStateController(self,scan_state_controller):
-		self.scan_state_controller = scan_state_controller
-		
-	def setSleepTime(self, tm):
-		self.sleepMeasureTime = tm
-		
-	def getSleepTime(self):
-		return self.sleepMeasureTime
-		
-	def setFakeScan(self, bool_val):
-		self.fake_scan = bool_val
-		
-	def getFakeScan(self):
-		return self.fake_scan
-		
-	def setUseTrigger(self, bool_val):
-		self.use_trigger = bool_val
-		
-	def getUseTrigger(self):
-		return self.use_trigger
-		
-	def makeShot(self):
-		print "Making shot..."
-		self.initChannels()
+    Note: if the there is an error in the machine, this class will not trigger
+    the beam. It seems that manually triggering the beam from StartMap will solve
+    the problem.
+    """
+    def __init__(self):
+        self.beam_trigger_wpv = None
+        self.test_pv = None
+        self.sleep_time = 0.1
+        self.fake_scan = False
+        self.use_trigger = True
 
-		state_cntrl = self.scan_state_controller 
-		messageTextField = null
+    def init_channels(self):
+        if not self.fake_scan:
+            if self.beam_trigger_wpv is None or self.test_pv is None:
+                self.beam_trigger_wpv = WrappedChannel("ICS_Tim:Gate_BeamOn:SSTrigger")
+                self.test_pv = WrappedChannel("MEBT_Diag:BPM01:amplitudeAvg")
+                self.test_pv.startMonitor()
+                time.sleep(2.0)
 
-		if(self.top_level_cotroller != null):
-			messageTextField = self.top_level_cotroller.getMessageTextField()
+    def set_sleep_time(self, sleep_time):
+        self.sleep_time = sleep_time
 
-		if(not self.fake_scan):
-			self.testPV.setValueChanged(false) 
-			time.sleep(0.01)
-			if(self.use_trigger):
-				print "triggering..."
-				self.beamTriggerWPV.setValue(1.0)
-				time.sleep(0.5)
-				self.beamTriggerWPV.setValue(0.0)
-				print "trigger value = {0}".format(self.beamTriggerWPV.getValue())
+    def get_sleep_time(self):
+        return self.sleep_time
 
-		#Return with fail if controller says to stop.
-		if(state_cntrl != null and state_cntrl.getShouldStop() == true): return false
+    def set_fake_scan(self, bool_val):
+        self.fake_scan = bool_val
 
-		time.sleep(self.sleepMeasureTime)
+    def get_fake_scan(self):
+        return self.fake_scan
 
-		#Return with fail if controller says to stop.
-		if(state_cntrl != null and state_cntrl.getShouldStop() == true): return false		
+    def set_use_trigger(self, bool_val):
+        self.use_trigger = bool_val
 
-		count = 0
-		time_sleep = 0.05
-		#if scan is real
-		if(not self.fake_scan):
-			#Keep looping as long as the testPV is hasn't changed.
-			while(self.testPV.valueChanged() == false):
-				
-				count = count + 1
+    def get_use_trigger(self):
+        return self.use_trigger
 
-				#Kill loop if controller says to stop.
-				if(state_cntrl != null and state_cntrl.getShouldStop() == true): return false	
+    def fire(self):
+        print("Making shot...")
+        self.init_channels()
 
-				time.sleep(time_sleep)
+        if not self.fake_scan:
+            self.test_pv.setValueChanged(False) 
+            time.sleep(0.01)
+            if self.use_trigger:
+                print("triggering...")
+                self.beam_trigger_wpv.setValue(1.0)
+                time.sleep(0.5)
+                self.beam_trigger_wpv.setValue(0.0)
+                print("trigger value = {0}".format(self.beam_trigger_wpv.getValue()))
 
-				#Kill loop if controller says to stop.
-				if(state_cntrl != null and state_cntrl.getShouldStop() == true): return false			
+        time.sleep(self.sleep_time)
 
-				if(count % 25 == 0): 
-					time_sleep = time_sleep+0.1
-				if(count > 25):
-					if(messageTextField != null):
-						messageTextField.setText("Attention!!! Something wrong. Please fire the beam manually! Bad count="+str(count))			
-
-			if(messageTextField != null):
-				messageTextField.setText("")
-		return true
-
+        count = 0
+        time_sleep = 0.05
+        if not self.fake_scan:
+            while not self.test_pv.valueChanged():
+                count += 1
+                time.sleep(time_sleep)
+                if count % 25 == 0: 
+                    time_sleep += 0.1
+                if count > 25:
+                    print("Something is wrong! Please fire the beam manually! Bad count = {}".format(count))
+        return True
