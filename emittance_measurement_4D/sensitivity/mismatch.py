2c3c7c4165c345dd8ef3382fb8fad2959c16d0df
austin-hoover <ahoover7@tennessee.edu>
2022-01-29 15:31:51 -0500

    Working version of study on mismatch vs. reconstruction errors
    The analysis notebook needs to be cleaned

--- emittance_measurement_4D/sensitivity/mismatch.py
+++ emittance_measurement_4D/sensitivity/mismatch.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import sys
 import os
 import math
+import pickle
 import random
 from Jama import Matrix
 
@@ -37,15 +38,12 @@ from lib.least_squares import lsq_linear
 from lib import utils
 
 
-utils.delete_files_not_folders('_output/')
-
-
 # Setup
 kinetic_energy = 1.0e9 # [eV]
 ws_ids = ['RTBT_Diag:WS20', 'RTBT_Diag:WS21', 'RTBT_Diag:WS23', 'RTBT_Diag:WS24']
 ref_ws_id = 'RTBT_Diag:WS24'
-n_trials = 1
-frac_error = 0.0
+n_trials = 100
+frac_error = 0.02
 controller = optics.PhaseController(ref_ws_id=ref_ws_id, 
                                     kinetic_energy=kinetic_energy)
 
@@ -65,73 +63,91 @@ default_fields = controller.get_fields(quad_ids)
 # Lattice Twiss parameters.
 eps_1 = 20.1e-6 # [m rad]
 eps_2 = 20.0e-6 # [m rad]
-alpha_x0 = controller.init_twiss['alpha_x']
-alpha_y0 = controller.init_twiss['alpha_y']
-beta_x0 = controller.init_twiss['beta_x']
-beta_y0 = controller.init_twiss['beta_y']
+rec_node_id = 'RTBT_Diag:BPM17'
+_, _, alpha_x0, alpha_y0, beta_x0, beta_y0, _, _ = controller.twiss(rec_node_id)
 
 # Move to new optics setting.
 mux = utils.put_angle_in_range(mux0 + utils.radians(45.0))
-muy = utils.put_angle_in_range(muy0 + utils.radians(45.0))
+muy = utils.put_angle_in_range(muy0 - utils.radians(45.0))
 controller.set_ref_ws_phases(mux, muy, verbose=1)
-tmats = [controller.transfer_matrix(ws_id) for ws_id in ws_ids]
+tmats = [controller.transfer_matrix(rec_node_id, ws_id) for ws_id in ws_ids]
 
 # Study error as function of mismatched beam Twiss parameters.
-dalpha = 0.05
-dbeta = 0.3
+dalpha_x_min = -0.1
+dalpha_x_max = +0.1
+dalpha_y_min = -0.1
+dalpha_y_max = +0.5
+dbeta_x_min = -0.1
+dbeta_x_max = +0.1
+dbeta_y_min = -0.1
+dbeta_y_max = +0.1
 n = 10
 
-alpha_x_min = alpha_x0 * (1.0 - dalpha)
-alpha_x_max = alpha_x0 * (1.0 + dalpha)
+alpha_x_min = alpha_x0 * (1.0 + dalpha_x_min)
+alpha_x_max = alpha_x0 * (1.0 + dalpha_x_max)
 alpha_xs = utils.linspace(alpha_x_min, alpha_x_max, n)
 
-alpha_y_min = alpha_y0 * (1.0 - dalpha)
-alpha_y_max = alpha_y0 * (1.0 + dalpha)
+alpha_y_min = alpha_y0 * (1.0 + dalpha_y_min)
+alpha_y_max = alpha_y0 * (1.0 + dalpha_y_max)
 alpha_ys = utils.linspace(alpha_y_min, alpha_y_max, n)
 
-beta_x_min = beta_x0 * (1.0 - dbeta)
-beta_x_max = beta_x0 * (1.0 + dbeta)
+beta_x_min = beta_x0 * (1.0 + dbeta_x_min)
+beta_x_max = beta_x0 * (1.0 + dbeta_x_max)
 beta_xs = utils.linspace(beta_x_min, beta_x_max, n)
 
-beta_y_min = beta_y0 * (1.0 - dbeta)
-beta_y_max = beta_y0 * (1.0 + dbeta)
+beta_y_min = beta_y0 * (1.0 + dbeta_y_min)
+beta_y_max = beta_y0 * (1.0 + dbeta_y_max)
 beta_ys = utils.linspace(beta_y_min, beta_y_max, n)
 
+print(alpha_xs)
+print(alpha_ys)
+print(beta_xs)
+print(beta_ys)
 
-emittances_list = [[[[[0.0, 0.0, 0.0, 0.0] 
-                      for i in range(n)] 
-                     for j in range(n)] 
-                    for k in range(n)]
-                   for l in range(n)]
 
+
+def init_array(n):
+    array = [[[[[0.0, 0.0, 0.0, 0.0] 
+                for i in range(n)] 
+               for j in range(n)] 
+              for k in range(n)] 
+             for l in range(n)]
+    return array
+
+emittance_means_list = init_array(n)
+emittance_stds_list = init_array(n)
+fail_rates = [[[[0.0 for i in range(n)] for j in range(n)] for k in range(n)] for l in range(n)]
 for i, alpha_x, in enumerate(alpha_xs):
     for j, alpha_y in enumerate(alpha_ys):
         for k, beta_x in enumerate(beta_xs):
+            print(i, j, k)
             for l, beta_y in enumerate(beta_ys):
-                print(i, j, k, l)
                 Sigma0 = uncoupled_matched_cov(alpha_x, alpha_y, beta_x, beta_y, eps_1, eps_2)
                 fail_rate, emittances = run_trials(Sigma0, tmats, n_trials, frac_error)
                 means = utils.mean_cols(emittances)
-                for m in range(4):
-                    emittances_list[i][j][k][l][m] = means[m]
-                means = utils.mean_cols(emittances)
                 stds = utils.std_cols(emittances)
+                for m in range(4):
+                    emittance_means_list[i][j][k][l][m] = means[m]
+                    emittance_stds_list[i][j][k][l][m] = stds[m]
+                fail_rates[i][j][k][l] = fail_rate
 
-import pickle
-output = open('_output/data/data.pkl', 'wb')
-pickle.dump(emittances_list, output)
-output.close()
-
-def write_array_to_file(array, filename):
-    file = open(filename, 'w')
-    for x in array:
-        file.write('{} '.format(x))
+def save(array, filename, pkl=False):
+    file = open(filename, 'wb' if pkl else 'w')
+    if pkl:
+        pickle.dump(array, file)
+    else:
+        for x in array:
+            file.write('{} '.format(x))
     file.close()
-    return
-    
-write_array_to_file(alpha_xs, '_output/data/alpha_xs.dat')
-write_array_to_file(alpha_xs, '_output/data/alpha_ys.dat')
-write_array_to_file(beta_xs, '_output/data/beta_xs.dat')
-write_array_to_file(beta_ys, '_output/data/beta_ys.dat')
+                    
+save(fail_rates, '_output/data/fail_rates.pkl', pkl=True)
+save(emittance_means_list, '_output/data/emittance_means.pkl', pkl=True)
+save(emittance_stds_list, '_output/data/emittance_stds.pkl', pkl=True)
+save(alpha_xs, '_output/data/alpha_xs.dat')
+save(alpha_ys, '_output/data/alpha_ys.dat')
+save(beta_xs, '_output/data/beta_xs.dat')
+save(beta_ys, '_output/data/beta_ys.dat')
+save([alpha_x0, alpha_y0, beta_x0, beta_y0], '_output/data/true_twiss.dat')
+save([eps_1, eps_2], '_output/data/true_emittances.dat')
 
 exit()
